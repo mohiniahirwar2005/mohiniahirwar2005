4c3198d3afbc02ae9eda82b6ef7e50d7f2494a9b
mohiniahirwar2005 <mohiniahirwar828@gmail.com>
2026-06-09 20:35:49 +0530

    tailwind

---
+++ project/CravingsReactApp/src/components/GridExample.jsx
@@ -0,0 +1,93 @@
+const GridExample = () => {
+  return (
+    <>
+      <div className="grid grid-cols-[100px_25%_auto] gap-3">
+        <div className="border">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit
+          doloremque ullam magnam nobis repellat quam totam maiores laboriosam
+          at est, perferendis impedit molestias voluptas dolorem fugiat saepe
+          nulla similique provident.
+        </div>
+        <div className="border">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit
+          doloremque ullam magnam nobis repellat quam totam maiores laboriosam
+          at est, perferendis impedit molestias voluptas dolorem fugiat saepe
+          nulla similique provident.
+        </div>
+        <div className="border">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit
+          doloremque ullam magnam nobis repellat quam totam maiores laboriosam
+          at est, perferendis impedit molestias voluptas dolorem fugiat saepe
+          nulla similique provident.
+        </div>
+        <div className="border">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit
+          doloremque ullam magnam nobis repellat quam totam maiores laboriosam
+          at est, perferendis impedit molestias voluptas dolorem fugiat saepe
+          nulla similique provident.
+        </div>
+        <div className="border">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit
+          doloremque ullam magnam nobis repellat quam totam maiores laboriosam
+          at est, perferendis impedit molestias voluptas dolorem fugiat saepe
+          nulla similique provident.
+        </div>
+        <div className="border">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit
+          doloremque ullam magnam nobis repellat quam totam maiores laboriosam
+          at est, perferendis impedit molestias voluptas dolorem fugiat saepe
+          nulla similique provident.
+        </div>
+        <div className="border">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit
+          doloremque ullam magnam nobis repellat quam totam maiores laboriosam
+          at est, perferendis impedit molestias voluptas dolorem fugiat saepe
+          nulla similique provident.
+        </div>
+        <div className="border">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit
+          doloremque ullam magnam nobis repellat quam totam maiores laboriosam
+          at est, perferendis impedit molestias voluptas dolorem fugiat saepe
+          nulla similique provident.
+        </div>
+        <div className="border">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit
+          doloremque ullam magnam nobis repellat quam totam maiores laboriosam
+          at est, perferendis impedit molestias voluptas dolorem fugiat saepe
+          nulla similique provident.
+        </div>
+
+        <div className="border">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit
+          doloremque ullam magnam nobis repellat quam totam maiores laboriosam
+          at est, perferendis impedit molestias voluptas dolorem fugiat saepe
+          nulla similique provident.
+        </div>
+        <div className="border">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit
+          doloremque ullam magnam nobis repellat quam totam maiores laboriosam
+          at est, perferendis impedit molestias voluptas dolorem fugiat saepe
+          nulla similique provident.
+        </div>
+        <div className="border">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit
+          doloremque ullam magnam nobis repellat quam totam maiores laboriosam
+          at est, perferendis impedit molestias voluptas dolorem fugiat saepe
+          nulla similique provident.
+        </div>
+        <div className="border">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit
+          doloremque ullam magnam nobis repellat quam totam maiores laboriosam
+          at est, perferendis impedit molestias voluptas dolorem fugiat saepe
+          nulla similique provident.
+        </div>
+        <div className="border">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit
+          doloremque ullam magnam nobis repellat quam totam maiores laboriosam
+          at est, perferendis impedit molestias voluptas dolorem fugiat saepe
+          nulla similique provident.
+        </div>
+      </div>
+    </>
+  );
+};
